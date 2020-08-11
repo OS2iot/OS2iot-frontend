43fc64004462be4fa0236536b773a974776ca935
Jeppe Elkjær Jørgensen <jej@netcompany.comen>
2020-08-11 16:36:32 +0200

    IOT-32 - Add new module and component for lora gateway. Setup nav and routing.

---
+++ src/app/views/administration-gateway/mine-lora-gateways/mine-lora-gateways.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mine-lora-gateways',
+  templateUrl: './mine-lora-gateways.component.html',
+  styleUrls: ['./mine-lora-gateways.component.scss']
+})
+export class MineLoraGatewaysComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
