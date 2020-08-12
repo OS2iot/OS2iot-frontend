deab35f9d4d2ee853e3a49ed31a2159062eda4b1
Jeppe Elkjær Jørgensen <jej@netcompany.comen>
2020-08-12 12:18:43 +0200

    IOT-33 add table, row and models

---
+++ src/app/views/administration-gateway/admin-lora-table-row/admin-lora-table-row.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Input, Output } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
+import { Gateway } from 'src/app/models/gateway';
+import { EventEmitter } from 'protractor';
+
+@Component({
+  selector: 'tr[app-admin-lora-table-row]',
+  templateUrl: './admin-lora-table-row.component.html',
+  styleUrls: ['./admin-lora-table-row.component.scss']
+})
+export class AdminLoraTableRowComponent implements OnInit {
+
+  @Input() gateway: Gateway;
+
+  //@Output() deleteGateway = new EventEmitter();
+
+  constructor(
+    public translate: TranslateService,
+    private router: Router
+    ) {
+      translate.use('da');
+  }
+
+  ngOnInit(): void {
+  }
+
+  clickDelete() {
+    //this.deleteGateway.emit(this.gateway.id);
+  }
+
+  navigateToEditPage(){
+    //this.router.navigate([])
+  }
+  
+
+}
