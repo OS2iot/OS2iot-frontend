8cad0facf9b5badcd66a072b7fc57c60929c6dda
Dennis Lundgaard <dsl@netcompany.com>
2020-08-20 10:54:01 +0200

    for merge

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 
@@ -7,7 +9,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    HttpClientModule
   ]
 })
 export class DashboardModule { }
