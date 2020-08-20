0d2c74ee76cf618320ef1ff7ab8e7b1b2fadaf6d
Jeppe Elkjær Jørgensen <jej@netcompany.com>
2020-08-20 09:10:23 +0200

    IOT-32 - tilføj felt til oprettelse af gateway

--- src/app/shared/_services/lora-gateway-question.service.ts
+++ src/app/shared/_services/lora-gateway-question.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { QuestionBase } from 'src/app/models/question-base';
-import { TextboxMediumQuestion } from 'src/app/models/question-textbox';
+import { TextboxMediumQuestion, TextboxSmallQuestion, TextareaLargeQuestion, TextareaMediumQuestion, NumberboxSmallQuestion } from 'src/app/models/question-textbox';
 import { of } from 'rxjs';
 
 @Injectable({
@@ -12,15 +12,67 @@ export class LoraGatewayQuestionService {
 
   getQuestions() {
     let questions: QuestionBase<string>[] = [
-      new TextboxMediumQuestion( {
+      new TextboxSmallQuestion( {
         key: 'name',
-        label: 'QUESTION.GIVE-LORA-GATEWAY-NAME',
+        label: 'QUESTION-LORA-GATEWAY.NAME',
         required: true,
         placeholder: 'QUESTION-LORA-GATEWAY.NAME-PLACEHOLDER',
         order: 1
+      }),
+      new TextareaMediumQuestion({
+        key: 'description',
+        label: 'QUESTION-LORA-GATEWAY.DESCRIPTION',
+        required: true,
+        type: 'test',
+        placeholder: 'QUESTION-LORA-GATEWAY.DESCRIPTION-PLACEHOLDER',
+        order: 2
+      }),
+      new TextboxSmallQuestion({
+        key: 'id',
+        label: 'QUESTION-LORA-GATEWAY.GATEWAYID',
+        required: true,
+        placeholder: 'QUESTION-LORA-GATEWAY.GATEWAYID-PLACEHOLDER',
+        order: 3
+      }),
+
+      new NumberboxSmallQuestion({
+        key: 'altitude',
+        label: 'QUESTION-LORA-GATEWAY.ALTITUDE',
+        required: false,
+        order: 4,
+        type: 'number',
+        placeholder: 'QUESTION-LORA-GATEWAY.ALTITUDE-PLACEHOLDER'
+      }),
+
+      new NumberboxSmallQuestion({
+        key: 'longitude',
+        label: 'QUESTION-LORA-GATEWAY.LONGITUDE',
+        required: false,
+        order: 5,
+        type: 'number',
+        placeholder: 'QUESTION-LORA-GATEWAY.LONGITUDE-PLACEHOLDER'
+      }),
+
+      new NumberboxSmallQuestion({
+        key: 'latitude',
+        label: 'QUESTION-LORA-GATEWAY.LATITUDE',
+        required: false,
+        order: 6,
+        type: 'number',
+        placeholder: 'QUESTION-LORA-GATEWAY.LATITUDE-PLACEHOLDER'
+      }),
+
+      new TextareaLargeQuestion({
+        key: 'metadata',
+        label: 'QUESTION-LORA-GATEWAY.METADATA',
+        required: false,
+        type: 'text',
+        placeholder: 'QUESTION-LORA-GATEWAY.METADATA-PLACEHOLDER',
+        order: 7
       })
-    ]
-    return of(questions.sort((a, b) => a.order - b.order))
+    ];
+
+    return of(questions.sort((a, b) => a.order - b.order));
   }
 
 }
