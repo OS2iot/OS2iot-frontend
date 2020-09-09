1e1dc0ea2a49acd793d3db6516fb646b234c96d9
Jeppe Elkjær Jørgensen <jej@netcompany.com>
2020-09-09 07:28:35 +0200

    IOT-263 add to navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,12 @@ import { ListLoraGatewayComponent } from './views/administration-gateway/list-lo
 import { EditGatewayComponent } from './views/administration-gateway/edit-gateway/edit-gateway.component';
 import { GatewayComponent } from './views/administration-gateway/gateway/gateway.component';
 import { DatatargetListComponent } from './views/datatarget/datatarget-list/datatarget-list.component';
+import { DatatargetEditComponent } from './views/datatarget/datatarget-edit/datatarget-edit.component';
+import { DatatargetComponent } from './views/datatarget/datatarget/datatarget.component';
+import { ProfilesComponent } from './profiles/profiles.component';
+import { AlleIotEnhederComponent } from './views/alle-iot-enheder/alle-iot-enheder/alle-iot-enheder.component';
+import { MyApplicationsComponent } from './my-applications/my-applications.component';
+import { PayloadDecoderComponent } from './payload-decoder/payload-decoder/payload-decoder.component';
 
 const routes: Routes = [
 
@@ -21,6 +27,8 @@ const routes: Routes = [
 
     },
     { path: 'datatarget', component: DatatargetListComponent },
+    { path: 'iot-devices', component: AlleIotEnhederComponent },
+    { path: 'payload-decoder', component: PayloadDecoderComponent },
     { path: 'profiles', loadChildren: () => import('./profiles/profiles.module').then(m => m.ProfilesModule) },
     { path: '', redirectTo: '/my-applications', pathMatch: 'full' },
     { path: '**', redirectTo: '/my-applications', pathMatch: 'full' },
