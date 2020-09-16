3c44e101e29fb4da26be3d66efe95e7ca6af5134
Allan Asp Christensen <aac@netcompany.com>
2020-09-16 14:03:34 +0200

    Tilføjelse af log ud

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService, AuthResponseData } from './auth.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -14,7 +15,7 @@ export class AuthComponent implements OnInit {
   error: string = null;
 
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -44,6 +45,8 @@ export class AuthComponent implements OnInit {
     });
 
     form.reset();
+
+    this.router.navigateByUrl('/my-applications')
 
   }
 }
