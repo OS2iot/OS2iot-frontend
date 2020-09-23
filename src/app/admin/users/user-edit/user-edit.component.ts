db133150b813ee609982e0e7e69b73054042f9d2
Jeppe Elkjær Jørgensen <jej@netcompany.comen>
2020-09-23 17:23:24 +0200

    IOT-477 fix. now get correct param

--- src/app/admin/users/user-edit/user-edit.component.ts
+++ src/app/admin/users/user-edit/user-edit.component.ts
@@ -44,7 +44,7 @@ export class UserEditComponent implements OnInit {
         this.title = translations['FORM.EDIT-USERS'];
         this.submitButton = translations['USERS.SAVE'];
       });
-    this.id = +this.route.snapshot.paramMap.get('org-id');
+    this.id = +this.route.snapshot.paramMap.get('user-id');
     if (this.id > 0) {
       this.getUser(this.id);
     }
