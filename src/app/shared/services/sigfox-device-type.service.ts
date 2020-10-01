75023fcb1373bd77ae6509d3167a701250b16581
Jeppe Elkjær Jørgensen <jej@netcompany.comen>
2020-10-01 15:24:34 +0200

    IOT-271 add device type service

---
+++ src/app/shared/services/sigfox-device-type.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { RestService } from './rest.service';
+import { Observable } from 'rxjs';
+import { SigfoxDeviceType } from '@shared/models/sigfox-device-type.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SigfoxGroupService {
+
+  private SIGFOXDEVICETYPEURL = 'sigfox-device-type';
+
+  constructor(private restService: RestService) { }
+
+  public getOne(id: string, params = {}): Observable<any> {
+    return this.restService.get(this.SIGFOXDEVICETYPEURL, params, id);
+  }
+
+  public getMultiple(organizationId: number): Observable<any> {
+    const body = {organizationId};
+    return this.restService.get(this.SIGFOXDEVICETYPEURL, body);
+  }
+
+  public post(sigfoxGroup: SigfoxDeviceType): Observable<any> {
+    return this.restService.post(this.SIGFOXDEVICETYPEURL, sigfoxGroup, { observe: 'response' });
+  }
+
+  public put(sigfoxGroup: SigfoxDeviceType): Observable<any> {
+    return this.restService.put(this.SIGFOXDEVICETYPEURL, sigfoxGroup, sigfoxGroup.id);
+  }
+}
