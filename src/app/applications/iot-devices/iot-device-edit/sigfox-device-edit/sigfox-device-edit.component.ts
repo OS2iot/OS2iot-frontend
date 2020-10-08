c6bfa9fdb12db70b94ca11832304237a36cec71c
Jeppe Elkjær Jørgensen <jej@netcompany.com>
2020-10-08 11:33:07 +0200

    IOT-53 add edit mode restrictions

--- src/app/applications/iot-devices/iot-device-edit/sigfox-device-edit/sigfox-device-edit.component.ts
+++ src/app/applications/iot-devices/iot-device-edit/sigfox-device-edit/sigfox-device-edit.component.ts
@@ -6,6 +6,7 @@ import { SigfoxService } from '@shared/services/sigfox.service';
 import { SharedVariableService } from '@shared/shared-variable/shared-variable.service';
 import { SigfoxContract } from '@shared/models/sigfox-contract.model';
 import { SigfoxDeviceType } from '@shared/models/sigfox-device-type.model';
+import { DeviceType } from '@shared/enums/device-type';
 @Component({
   selector: 'app-sigfox-device-edit',
   templateUrl: './sigfox-device-edit.component.html',
@@ -21,6 +22,7 @@ export class SigfoxDeviceEditComponent implements OnInit {
   public formFailedSubmit = false;
   public sigfoxContract: SigfoxContract;
   public sigfoxDeviceTypes: SigfoxDeviceType[] = [];
+  public editMode = false;
 
   constructor(
     public translate: TranslateService,
@@ -33,6 +35,9 @@ export class SigfoxDeviceEditComponent implements OnInit {
     this.translate.use('da');
     this.organizationId = this.sharedVariable.getSelectedOrganisationId();
     this.getGroups();
+    if (this.iotDevice?.id) {
+      this.editMode = true;
+    }
   }
 
   getGroups() {
@@ -44,8 +49,10 @@ export class SigfoxDeviceEditComponent implements OnInit {
 
   changeContractAndDeviceType() {
       this.resetModelOnChangedGroup();
-      this.getContracts(this.iotDevice?.sigfoxSettings?.groupId);
-      this.getDeviceTypes(this.iotDevice?.sigfoxSettings?.groupId);
+      if (this.iotDevice.sigfoxSettings.groupId) {
+        this.getContracts(this.iotDevice?.sigfoxSettings?.groupId);
+        this.getDeviceTypes(this.iotDevice?.sigfoxSettings?.groupId);
+      }
   }
 
   resetModelOnChangedGroup() {
