f64d1076dd8cf10711b6ded1e5348223e43e9a78
Jeppe Elkjær Jørgensen <jej@netcompany.com>
2020-10-14 14:27:12 +0200

    IOT-651 fix routing

--- src/app/admin/permission/permission-list/permission-tabel/permission-row/permission-row.component.ts
+++ src/app/admin/permission/permission-list/permission-tabel/permission-row/permission-row.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PermissionResponse } from '@app/admin/permission/permission.model';
 
 @Component({
@@ -10,12 +11,19 @@ export class PermissionRowComponent implements OnInit {
   @Input() permission: PermissionResponse;
   @Output() innerDeletePermission = new EventEmitter();
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void { }
 
   clickDelete() {
     console.log("row")
     this.innerDeletePermission.emit(this.permission.id);
+  }
+
+  routeToPermissions() {
+    this.router.navigate(['admin/permissions', this.permission.id]);
   }
 }
