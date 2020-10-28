69ea83337c0d0dc6246686be2c0edb364bea0efe
Jeppe Elkjær Jørgensen <jej@netcompany.comen>
2020-10-28 15:18:54 +0100

    IOT-403 add to navbar, add routing, moved enums

--- src/app/ngsi-ld-device-model/Enums/device-function.enum.ts
+++
@@ -1,8 +0,0 @@
-export enum DeviceFunction {
-    LEVELCONTROL = 'levelControl',
-    SENSING = 'sensing',
-    ONOFF = 'onOff',
-    OPENCLOSE = 'openClose',
-    METERING = 'metering',
-    EVENTNOTIFICATION = 'eventNotification'
-}
