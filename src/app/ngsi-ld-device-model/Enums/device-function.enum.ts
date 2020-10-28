79ba455a778f338bb59e1d91ae708c52614b164d
Jeppe Elkjær Jørgensen <jej@netcompany.comen>
2020-10-28 14:44:07 +0100

    IOT-403 add ngsild module, add enums and model

---
+++ src/app/ngsi-ld-device-model/Enums/device-function.enum.ts
@@ -0,0 +1,8 @@
+export enum DeviceFunction {
+    LEVELCONTROL = 'levelControl',
+    SENSING = 'sensing',
+    ONOFF = 'onOff',
+    OPENCLOSE = 'openClose',
+    METERING = 'metering',
+    EVENTNOTIFICATION = 'eventNotification'
+}
