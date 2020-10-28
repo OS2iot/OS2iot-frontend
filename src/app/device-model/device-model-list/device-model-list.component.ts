f32163d2fa31016d4808e150f5e17389eb64ed6d
Jeppe Elkjær Jørgensen <jej@netcompany.comen>
2020-10-28 17:27:10 +0100

    IOT-403 add table

--- src/app/device-model/device-model-list/device-model-list.component.ts
+++ src/app/device-model/device-model-list/device-model-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-device-model-list',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeviceModelListComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private translate: TranslateService
+  ) { }
 
   ngOnInit(): void {
   }
