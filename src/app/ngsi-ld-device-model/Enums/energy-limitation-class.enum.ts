637e580076a5a9f211794f47246fb63f92e289ca
Jeppe Elkjær Jørgensen <jej@netcompany.comen>
2020-10-28 14:44:07 +0100

    IOT-403 add ngsild module, add enums and model

---
+++ src/app/ngsi-ld-device-model/Enums/energy-limitation-class.enum.ts
@@ -0,0 +1,6 @@
+export enum EnergyLimitationClass {
+    E0 = 'Event energy-limited',
+    E1 = 'Period energy-limited',
+    E2 = 'Lifetime energy-limited',
+    E9 = 'No direct quantitative limitations to available energy'
+}
