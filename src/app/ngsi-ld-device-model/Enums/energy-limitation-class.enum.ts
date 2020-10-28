dec6ef8598921214323a5999ce731b442bc1ea09
Jeppe Elkjær Jørgensen <jej@netcompany.comen>
2020-10-28 15:18:54 +0100

    IOT-403 add to navbar, add routing, moved enums

--- src/app/ngsi-ld-device-model/Enums/energy-limitation-class.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum EnergyLimitationClass {
-    E0 = 'Event energy-limited',
-    E1 = 'Period energy-limited',
-    E2 = 'Lifetime energy-limited',
-    E9 = 'No direct quantitative limitations to available energy'
-}
