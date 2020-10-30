fb85feb600314f20bdbb260c48a7ea6d93268f44
Jeppe Elkjær Jørgensen <jej@netcompany.comen>
2020-10-30 14:33:24 +0100

    IOT-783 corrected routing path

--- src/app/applications/applications-list/applications-table/applications-table.component.ts
+++ src/app/applications/applications-list/applications-table/applications-table.component.ts
@@ -17,9 +17,6 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router, ActivatedRoute } from '@angular/router';
-import { ApplicationService } from '@applications/application.service';
-import { SharedVariableService } from '@shared/shared-variable/shared-variable.service';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-applications-table',
@@ -73,9 +70,5 @@ export class ApplicationsTableComponent implements OnInit, AfterViewInit, OnChan
 
   navigateToEditPage() {
     this.router.navigate(['edit-application', this.application.id]);
-  }
-
-  onViewDetails(id) {
-    this.router.navigate([id], { relativeTo: this.route });
   }
 }
