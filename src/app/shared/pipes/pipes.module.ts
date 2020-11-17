f6d6a6210aad96222b60f0a8296854d3947be63a
Allan Asp Christensen <aac@netcompany.com>
2020-11-17 10:32:44 +0100

    init filtrering

--- src/app/shared/pipes/pipes.module.ts
+++ src/app/shared/pipes/pipes.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { YesNoPipe } from './yesNo.pipe';
 import { ActiveDeactivePipe } from './activeDeactive.pipe';
 import { isGlobalAdminPipe } from './is-global-admin.pipe';
+import { OrganisationFilterPipe } from './organisation-filter.pipe';
 
 @NgModule({
     declarations: [
         isGlobalAdminPipe,
         ActiveDeactivePipe,
         YesNoPipe,
+        OrganisationFilterPipe,
     ],
     imports: [
         CommonModule
