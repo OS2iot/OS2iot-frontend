c0f84330940b188930c912850a13e3114e90a86a
Allan Asp Christensen <aac@netcompany.com>
2020-11-18 13:45:08 +0100

    Refactor gatewayservice to parent component

--- src/app/gateway/gateway-list/gateway-list.component.ts
+++ src/app/gateway/gateway-list/gateway-list.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Organisation } from '@app/admin/organisation/organisation.model';
-import { OrganisationService } from '@app/admin/organisation/organisation.service';
 import { TranslateService } from '@ngx-translate/core';
 import { Sort } from '@shared/models/sort.model';
 import { ChirpstackGatewayService } from '@shared/services/chirpstack-gateway.service';
 import * as moment from 'moment';
 import { Subscription } from 'rxjs';
 import { Gateway, GatewayResponseMany } from '../gateway.model';
-import { GatewayTableComponent } from '../gateway-table/gateway-table.component';
-import { FormControl, FormGroup } from '@angular/forms';
 import { MatOption } from '@angular/material/core';
 import { MatSelect } from '@angular/material/select';
+import { DeleteDialogService } from '@shared/components/delete-dialog/delete-dialog.service';
+import { MeService } from '@shared/services/me.service';
 
 
 @Component({
@@ -18,9 +17,8 @@ import { MatSelect } from '@angular/material/select';
   templateUrl: './gateway-list.component.html',
   styleUrls: ['./gateway-list.component.scss']
 })
-export class GatewayListComponent implements OnInit, OnDestroy {
-
-  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+export class GatewayListComponent implements OnInit, OnChanges, OnDestroy {
+  isLoadingResults = true;
   @ViewChild('select') select: MatSelect;
   allSelected = false;
 
@@ -39,14 +37,25 @@ export class GatewayListComponent implements OnInit, OnDestroy {
   organisation: Organisation;
   orgSubscribtion: Subscription;
 
+  private deleteDialogSubscription: Subscription;
+  public pageOffset = 0;
+  public pageTotal: number;
+
   constructor(
     public translate: TranslateService,
-    private chirpstackGatewayService: ChirpstackGatewayService) {
+    private chirpstackGatewayService: ChirpstackGatewayService,
+    private deleteDialogService: DeleteDialogService,
+    private meService: MeService) {
     translate.use('da');
     moment.locale('da');
   }
 
   ngOnInit(): void {
+    this.getGateways();
+  }
+
+  ngOnChanges() {
+    this.getGateways();
   }
 
   toggleAllSelection() {
@@ -72,6 +81,8 @@ export class GatewayListComponent implements OnInit, OnDestroy {
         (gateways: GatewayResponseMany) => {
           this.gateways = gateways.result;
           this.mapToCoordinateList();
+          this.setCanEdit();
+          this.isLoadingResults = false;
         }
       );
   }
@@ -105,14 +116,42 @@ export class GatewayListComponent implements OnInit, OnDestroy {
     console.log('getCoordinateList called');
   }
 
-  ngOnDestroy(): void {
+  gatewayStatus(gateway: Gateway): boolean {
+    return this.chirpstackGatewayService.isGatewayActive(gateway);
+  }
+
+  deleteGateway(id: string) {
+    this.deleteDialogSubscription = this.deleteDialogService.showSimpleDeleteDialog().subscribe(
+      (response) => {
+        if (response) {
+          this.chirpstackGatewayService.delete(id).subscribe((response) => {
+            if (response.ok && response.body.success === true) {
+              this.getGateways();
+            }
+          });
+        } else {
+          console.log(response);
+        }
+      }
+    );
+  }
+
+  setCanEdit() {
+    this.gateways.forEach(
+      (gateway) => {
+        gateway.canEdit = this.meService.canWriteInTargetOrganization(gateway.internalOrganizationId);
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    // prevent memory leak by unsubscribing
     if (this.gatewaySubscription) {
       this.gatewaySubscription.unsubscribe();
     }
-  }
-
-  gatewayStatus(gateway: Gateway): boolean {
-    return this.chirpstackGatewayService.isGatewayActive(gateway);
+    if (this.deleteDialogSubscription) {
+      this.deleteDialogSubscription.unsubscribe();
+    }
   }
 
 }
