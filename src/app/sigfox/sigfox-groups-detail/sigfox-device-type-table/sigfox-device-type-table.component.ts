e874df016e8a366a8d9a6a828d512b36dedc61d0
jeppeej <jeppeej@gmail.com>
2020-12-09 09:08:11 +0100

    IOT-1126 solved table paginator issue.

--- src/app/sigfox/sigfox-groups-detail/sigfox-device-type-table/sigfox-device-type-table.component.ts
+++ src/app/sigfox/sigfox-groups-detail/sigfox-device-type-table/sigfox-device-type-table.component.ts
@@ -6,14 +6,13 @@ import { environment } from '@environments/environment';
 import { TranslateService } from '@ngx-translate/core';
 import { SigfoxDeviceType } from '@shared/models/sigfox-device-type.model';
 import { SigfoxGroup } from '@shared/models/sigfox-group.model';
-import { SigfoxService } from '@shared/services/sigfox.service';
 
 @Component({
   selector: 'app-sigfox-device-type-table',
   templateUrl: './sigfox-device-type-table.component.html',
   styleUrls: ['./sigfox-device-type-table.component.scss']
 })
-export class SigfoxDeviceTypeTableComponent implements OnInit, AfterViewInit, OnChanges {
+export class SigfoxDeviceTypeTableComponent implements OnInit, OnChanges {
   @Input() sigfoxDevices: SigfoxDeviceType[];
   public dataSource = new MatTableDataSource<SigfoxDeviceType>();
 
@@ -26,30 +25,20 @@ export class SigfoxDeviceTypeTableComponent implements OnInit, AfterViewInit, On
 
   constructor(
     private translate: TranslateService,
-
     private route: ActivatedRoute,
-    private router: Router
-  ) {
+    private router: Router) {
     this.translate.use('da');
   }
 
   ngOnInit(): void {
-
-
   }
 
   ngOnChanges() {
     if (this.sigfoxDevices) {
-      this.dataSource = new MatTableDataSource(this.sigfoxDevices);
+      this.dataSource.data = this.sigfoxDevices;
       this.dataSource.paginator = this.paginator;
-      this.isLoadingResults = false;
       this.resultsLength = this.sigfoxDevices.length;
     }
-  }
-
-
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
   }
 
   editDeviceType(row: any) {
