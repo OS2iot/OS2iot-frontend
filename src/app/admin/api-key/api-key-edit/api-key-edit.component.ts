21f728fb7a572582763e9b114ce8493cad2e8beb
Aram Al-Sabti <afa@iterator-it.dk>
2021-11-30 18:25:50 +0100

    Show api keys

--- src/app/admin/api-key/api-key-edit/api-key-edit.component.ts
+++ src/app/admin/api-key/api-key-edit/api-key-edit.component.ts
@@ -1,15 +1,81 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PermissionResponse } from '@app/admin/permission/permission.model';
+import { TranslateService } from '@ngx-translate/core';
+import { BackButton } from '@shared/models/back-button.model';
+import { ApiKeyRequest } from '../api-key.model';
+import { ApiKeyService } from '../api-key.service';
 
 @Component({
   selector: 'app-api-key-edit',
   templateUrl: './api-key-edit.component.html',
-  styleUrls: ['./api-key-edit.component.scss']
+  styleUrls: ['./api-key-edit.component.scss'],
 })
 export class ApiKeyEditComponent implements OnInit {
+  apiKeyRequest = new ApiKeyRequest();
+  public backButton: BackButton = {
+    label: '',
+    routerLink: ['admin', 'api-key'],
+  };
+  public title = '';
+  public submitButton = '';
+  private apiKeyId: number;
+  public errorMessage: string;
+  public errorMessages: string[];
+  public errorFields: string[];
+  public formFailedSubmit = false;
+  public isEditMode = false;
+  public permissions: PermissionResponse[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private translate: TranslateService,
+    private route: ActivatedRoute,
+    private location: Location,
+    private apiKeyService: ApiKeyService
+  ) {
+    translate.use('da');
   }
 
+  ngOnInit(): void {
+    this.translate
+      .get(['NAV.API-KEY', 'FORM.EDIT-API-KEY', 'API-KEY.EDIT.SAVE'])
+      .subscribe((translations) => {
+        this.backButton.label = translations['NAV.API-KEY'];
+        this.title = translations['FORM.EDIT-API-KEY'];
+        this.submitButton = translations['API-KEY.EDIT.SAVE'];
+      });
+
+    this.apiKeyId = +this.route.snapshot.paramMap.get('api-key-id');
+
+    if (this.apiKeyId > 0) {
+      // TODO: Fetch current api key
+      this.isEditMode = true;
+    }
+  }
+
+  onSubmit(): void {
+    if (this.apiKeyId) {
+      this.update();
+    } else {
+      this.create();
+    }
+  }
+
+  private create(): void {
+    // TODO: CREATE
+  }
+
+  private update(): void {
+    // TODO: UPDATE
+    this.routeBack();
+  }
+
+  public compare(o1: any, o2: any): boolean {
+    return o1 === o2;
+  }
+
+  routeBack(): void {
+    this.location.back();
+  }
 }
