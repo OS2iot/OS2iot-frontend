7112f40489785ca7a8b6cd2dc696794e5f9a1e15
August Andersen <aha@iterator-it.dk>
2022-01-06 14:50:30 +0100

    merge conflicting

--- src/app/applications/application-detail/application-detail.component.ts
+++ src/app/applications/application-detail/application-detail.component.ts
@@ -1,4 +1,10 @@
-import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Application } from '@applications/application.model';
@@ -12,95 +18,106 @@ import { MeService } from '@shared/services/me.service';
 import { Subscription } from 'rxjs';
 
 @Component({
-    selector: 'app-application',
-    templateUrl: './application-detail.component.html',
-    styleUrls: ['./application-detail.component.scss'],
+  selector: 'app-application',
+  templateUrl: './application-detail.component.html',
+  styleUrls: ['./application-detail.component.scss'],
 })
 export class ApplicationDetailComponent implements OnInit, OnDestroy {
-    @Output() deleteApplication = new EventEmitter();
-    public applicationsSubscription: Subscription;
-    private deleteDialogSubscription: Subscription;
-    public application: Application;
-    public backButton: BackButton = { label: '', routerLink: '/applications' };
-    public id: number;
-    public pageLimit = environment.tablePageSize;
-    public dropdownButton: DropdownButton;
-    public errorMessage: string;
-    public canEdit = false;
+  @Output() deleteApplication = new EventEmitter();
+  public applicationsSubscription: Subscription;
+  private deleteDialogSubscription: Subscription;
+  public application: Application;
+  public backButton: BackButton = { label: '', routerLink: '/applications' };
+  public id: number;
+  public pageLimit = environment.tablePageSize;
+  public dropdownButton: DropdownButton;
+  public errorMessage: string;
+  public canEdit = false;
 
-    constructor(
-        private applicationService: ApplicationService,
-        private route: ActivatedRoute,
-        public translate: TranslateService,
-        public router: Router,
-        private meService: MeService,
-        private titleService: Title,
-        private deleteDialogService: DeleteDialogService
-    ) { }
+  constructor(
+    private applicationService: ApplicationService,
+    private route: ActivatedRoute,
+    public translate: TranslateService,
+    public router: Router,
+    private meService: MeService,
+    private titleService: Title,
+    private deleteDialogService: DeleteDialogService
+  ) {}
 
-    ngOnInit(): void {
-        this.id = +this.route.snapshot.paramMap.get('id');
-        if (this.id) {
-            this.bindApplication(this.id);
-            this.dropdownButton = {
-                label: '',
-                editRouterLink: '../../edit-application/' + this.id,
-                isErasable: true,
-            };
+  ngOnInit(): void {
+    this.id = +this.route.snapshot.paramMap.get('id');
+    if (this.id) {
+      this.bindApplication(this.id);
+      this.dropdownButton = {
+        label: '',
+        editRouterLink: '../../edit-application/' + this.id,
+        isErasable: true,
+      };
 
-            console.log(this.id);
-        }
+      console.log(this.id);
+    }
 
-        this.translate.get(['NAV.APPLICATIONS', 'APPLICATION-TABLE-ROW.SHOW-OPTIONS', 'TITLE.APPLICATION'])
-            .subscribe(translations => {
-                this.backButton.label = translations['NAV.APPLICATIONS'];
-                this.dropdownButton.label = translations['APPLICATION-TABLE-ROW.SHOW-OPTIONS'];
-                this.titleService.setTitle(translations['TITLE.APPLICATION']);
+    this.translate
+      .get([
+        'NAV.APPLICATIONS',
+        'APPLICATION-TABLE-ROW.SHOW-OPTIONS',
+        'TITLE.APPLICATION',
+      ])
+      .subscribe((translations) => {
+        this.backButton.label = translations['NAV.APPLICATIONS'];
+        this.dropdownButton.label =
+          translations['APPLICATION-TABLE-ROW.SHOW-OPTIONS'];
+        this.titleService.setTitle(translations['TITLE.APPLICATION']);
+      });
+    this.canEdit = this.meService.canWriteInTargetOrganization();
+  }
+
+  onDeleteApplication() {
+    let message: string;
+    if (this.applicationHasDevices()) {
+      message = this.translate.instant('APPLICATION.DELETE-HAS-DEVICES-PROMPT');
+    }
+
+    this.deleteDialogSubscription = this.deleteDialogService
+      .showSimpleDialog(message)
+      .subscribe((response) => {
+        if (response) {
+          this.applicationService
+            .deleteApplication(this.application.id)
+            .subscribe((response) => {
+              if (response.ok && response.body.affected > 0) {
+                console.log(
+                  'delete application with id:' + this.application.id.toString()
+                );
+                this.router.navigate(['applications']);
+              } else {
+                this.errorMessage = response?.error?.message;
+              }
             });
-        this.canEdit = this.meService.canWriteInTargetOrganization();
-    }
-
-    onDeleteApplication() {
-        let message: string;
-        if (this.applicationHasDevices()) {
-            message = this.translate.instant('APPLICATION.DELETE-HAS-DEVICES-PROMPT');
+        } else {
+          console.log(response);
         }
+      });
+  }
 
-        this.deleteDialogSubscription = this.deleteDialogService.showSimpleDialog(message).subscribe(
-            (response) => {
-                if (response) {
-                    this.applicationService.deleteApplication(this.application.id).subscribe((response) => {
-                        if (response.ok && response.body.affected > 0) {
-                            console.log('delete application with id:' + this.application.id.toString());
-                            this.router.navigate(['applications']);
-                        } else {
-                            this.errorMessage = response?.error?.message;
-                        }
-                    });
-                } else {
-                    console.log(response);
-                }
-            }
-        );
-    }
+  applicationHasDevices(): boolean {
+    return this.application.iotDevices?.length > 0;
+  }
 
-    applicationHasDevices(): boolean {
-        return this.application.iotDevices?.length > 0;
-    }
+  bindApplication(id: number): void {
+    this.applicationsSubscription = this.applicationService
+      .getApplication(id)
+      .subscribe((application) => {
+        this.application = application;
+      });
+  }
 
-    
-    bindApplication(id: number): void {
-        this.applicationsSubscription = this.applicationService.getApplication(id).subscribe((application) => {
-            this.application = application;
-        });
+  ngOnDestroy() {
+    if (this.applicationsSubscription) {
+      this.applicationsSubscription.unsubscribe();
     }
-
-    ngOnDestroy() {
-        if (this.applicationsSubscription) {
-            this.applicationsSubscription.unsubscribe();
-        }
-        if (this.deleteDialogSubscription) {
-            this.deleteDialogSubscription.unsubscribe();
-        }
+    if (this.deleteDialogSubscription) {
+      this.deleteDialogSubscription.unsubscribe();
     }
+  }
 }
