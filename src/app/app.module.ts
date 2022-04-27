3dbabfd96ed9abfafb74f067a5479bf2570dc541
AramAlsabti <92869496+AramAlsabti@users.noreply.github.com>
2022-04-27 17:17:40 +0200

    Show signal strength (#81)
    * Added columns. Error in lorawan component. Branch blocked until further notice
    
    * Fix TS error
    
    * Add signals to message. Not tested
    
    * Init graph and tabs on device detail page
    
    * Re-design device details page with tabs and history
    
    * Remove lorawan field component. Data is shown now shown under history
    
    * Update data rate colors. Limit to DR7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -69,7 +69,7 @@ export function tokenGetter() {
                 tokenGetter
             },
         }),
-        MonacoEditorModule.forRoot()
+        MonacoEditorModule.forRoot(),
     ],
     bootstrap: [AppComponent],
     exports: [TranslateModule],
--- src/app/shared/helpers/record.helper.ts
+++ src/app/shared/helpers/record.helper.ts
@@ -1,8 +1,9 @@
 export const recordToEntries = <T extends Record<string, unknown>>(
-  record: T
+  record: T,
+  isEnum = true,
 ) => {
   return Object.keys(record)
-    .filter((entry) => isNaN(Number(entry)))
+    .filter((entry) => !isEnum || isNaN(Number(entry)))
     .map((key: keyof typeof record) => ({
       key,
       value: record[key],
