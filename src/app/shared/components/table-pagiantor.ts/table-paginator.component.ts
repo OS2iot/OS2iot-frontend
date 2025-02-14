3e2a6409e1bc4a986eb317e7f806ca0a08008399
Lauest <nla@iterator-it.dk>
2025-02-14 13:48:34 +0100

    using mat-icons in table-paginator.component

--- src/app/shared/components/table-pagiantor.ts/table-paginator.component.ts
+++ src/app/shared/components/table-pagiantor.ts/table-paginator.component.ts
@@ -1,11 +1,12 @@
 import { NgClass, NgFor } from "@angular/common";
-import { AfterViewChecked, Component, Input, OnInit, ViewEncapsulation } from "@angular/core";
+import { AfterViewChecked, Component, Input, OnInit } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatOptionModule } from "@angular/material/core";
 import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatIconModule } from "@angular/material/icon";
+import { MatIconModule, MatIconRegistry } from "@angular/material/icon";
 import { MatPaginator } from "@angular/material/paginator";
 import { MatSelectModule } from "@angular/material/select";
+import { DomSanitizer } from "@angular/platform-browser";
 
 export interface Option {
   value: number;
@@ -18,12 +19,30 @@ export interface Option {
   styleUrls: ["./table-paginator.component.scss"],
   standalone: true,
   imports: [NgFor, MatIconModule, MatFormFieldModule, MatSelectModule, MatOptionModule, MatButtonModule, NgClass],
-  encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class TablePaginatorComponent implements OnInit, AfterViewChecked {
   ngOnInit(): void {}
   @Input() paginator: MatPaginator;
 
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
+    this.matIconRegistry.addSvgIcon(
+      "left-angle",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("assets/images/angle-left.svg"),
+      {}
+    );
+
+    this.matIconRegistry.addSvgIcon(
+      "right-angle",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("assets/images/angle-right.svg"),
+      {}
+    );
+
+    this.matIconRegistry.addSvgIcon(
+      "down-angle",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("assets/images/angle-down.svg"),
+      {}
+    );
+  }
   options: Option[] = [
     { value: 25, label: "25 pr. side" },
     { value: 50, label: "50 pr. side" },
