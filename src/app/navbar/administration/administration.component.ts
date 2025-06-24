d2f62106e1748c20f3023ab8e0fb660976fd0e29
August Andersen <aha@iterator-it.dk>
2025-06-24 14:44:35 +0200

    Changed name and check for who should be able to see administrationNavBar.

--- src/app/navbar/administration/administration.component.ts
+++ src/app/navbar/administration/administration.component.ts
@@ -1,21 +1,29 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { UserResponse } from "@app/admin/users/user.model";
+import { OrganizationAccessScope } from "@shared/enums/access-scopes";
 import { MeService } from "@shared/services/me.service";
 import { SharedVariableService } from "@shared/shared-variable/shared-variable.service";
 
 @Component({
-  selector: "app-global-admin",
-  templateUrl: "./global-admin.component.html",
-  styleUrls: ["./global-admin.component.scss"],
+  selector: "app-administration",
+  templateUrl: "./administration.component.html",
+  styleUrls: ["./administration.component.scss"],
 })
-export class GlobalAdminComponent implements OnInit {
+export class AdministrationComponent implements OnInit {
   public user: UserResponse;
   public isGlobalAdmin = false;
+
+  public isUserAdmin = false;
+  @Input() orgId: number;
 
   constructor(private sharedVariableService: SharedVariableService, private meService: MeService) {}
 
   ngOnInit(): void {
     this.user = this.sharedVariableService.getUserInfo().user;
     this.isGlobalAdmin = this.meService.hasGlobalAdmin();
+    this.isUserAdmin = this.meService.hasAccessToTargetOrganization(
+      OrganizationAccessScope.UserAdministrationWrite,
+      this.orgId
+    );
   }
 }
